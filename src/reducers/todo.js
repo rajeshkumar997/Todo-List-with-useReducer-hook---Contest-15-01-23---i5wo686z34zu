7677b14580ec3df1f31dfcede464eb10db2df81e
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-15 17:32:55 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,6 +1,16 @@
 const todoReducer = (state, action) => {
     switch (action.type) {
-
+        CASE "ADD":
+        return [...state,action.payload];
+        return state;
+            
+        CASE "DELETE":
+        return state.filter((item) =>{
+            if(item.id != action.payload.id){
+                return state;
+            }
+        });
+        default:return state;
     }
 }
 
