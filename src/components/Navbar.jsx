9db7bc3ced748676568199dcac2fc5eceead6ccd
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-15 17:45:51 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/App.css";
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="logo">Todo List</div>
+      <button className="theme">Theme</button>
+    </div>
+  );
+};
+
+export default Navbar;
